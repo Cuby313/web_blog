006bb9ee971672d7ef2f489c71fe175d909ee763
Cuby313 <Krille.12.0@hotmail.se>
2025-04-02 20:02:41 +0200

    Main page okay

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -5,6 +5,9 @@ calls to user interactions.
 
 document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.getElementById('loginForm');
+  const createPostForm = document.getElementById('createPostForm');
+  const blogCardGroup = document.querySelector('.blog-card-group'); 
+
   if (loginForm) {
     loginForm.addEventListener('submit', async (e) => {
       e.preventDefault();
@@ -15,16 +18,58 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  const createPostForm = document.getElementById('createPostForrm');
   if (createPostForm) {
     createPostForm.addEventListener('submit', async(e) => {
       e.preventDefault();
-      const title = document.getElemenyById('title').value;
-      const content = document.getElemenyById('content').value;
-      const result = await api.createPost({ title, content });
-      console.log(result);
+      const title = document.getElementById('title').value;
+      const content = document.getElementById('content').value;
+      try { 
+        const result = await api.createPost({ title, content });
+        console.log('Post created:', result);
+
+        addNewPostToDOM(result);
+
+        createPostForm.reset();
+      } catch (error) {
+        console.error('Error while creating post:', error);
+      }
     });
+}
+
+// Add a new post to the DOM
+function addNewPostToDOM(post) {
+  const blogCard = document.createElement('div');
+  blogCard.className = 'blog-card';
+
+  blogCard.innerHTML = `
+  <div class="blog-card-banner">
+    <img src="${post.picture}" alt="Front logo machu" width="250" class="blog-banner-img">
+  </div>
+  <div class="blog-content-wrapper">
+    <button class="blog-topic text-tiny">${post.topic}</button>
+    <h3 class="h3"><a href="#">${post.title}</a></h3>
+    <p class="blog-text">${post.content}</p>
+    <div class="wrapper-flex">
+      <div class="wrapper">
+        <p class="text-sm"><time datetime="${new Date().toISOString().split('T')[0]}"> ${new Date().toLocaleDateString()}</time></p>
+      </div>
+    </div>
+  </div>
+  `;
+
+  blogCardGroup.insertBefore(blogCard, blogCardGroup.firstChild);
+}
+
+async function loadInitialPosts() {
+  try {
+    const posts = await api.getPosts();
+    posts.forEach(post => addNewPostToDOM(post));
+  } catch (error) {
+    console.error('Error while loading posts', error);
   }
+}
+
+loadInitialPosts();
 });
 
 // navbar variables
